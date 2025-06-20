919f229156a799b4f7084b65623fad434b33b97a
Arya Sabale <aryastudy04@gmail.com>
2025-06-20 13:41:57 +0530

    Use deployed backend API URL for cros

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000' })); // Allow frontend access
+app.use(cors({ origin: 'https://notes-app-frontend.vercel.app' })); // Allow frontend access
 app.use(express.json()); // Parse incoming JSON
 
 // Routes
